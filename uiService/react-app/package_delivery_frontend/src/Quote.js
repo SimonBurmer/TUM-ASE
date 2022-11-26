ea8d66a1cccc9275f975402425d5bb2fad01440d
Peter Johanns <p-johanns-cs@bitbucket.com>
2022-11-26 16:05:32 +0100

    quote

---
+++ uiService/react-app/package_delivery_frontend/src/Quote.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Button from "@mui/material/Button";
+
+// function Quote() {
+//     return (
+//         <div className="App">
+//             <h1>
+//                 Welcome Felix! Welcome Peter!
+//             </h1>
+//         </div>
+//     );
+// }
+//
+// export default Quote;
+
+function Quote() {
+    return (
+        <div className="App">
+            <p>My favorite quote.</p>
+            <Button variant="contained">My MUI Button</Button>
+        </div>
+    );
+}
+
+export default Quote;
