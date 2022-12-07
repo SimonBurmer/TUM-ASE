31934c11883ad1c3fc27cd806202335adef4886e
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2022-12-07 11:41:56 +0100

    Add service registry

--- deliveryService/src/main/java/edu/tum/ase/project/DeliveryServiceApplication.java
+++ deliveryService/src/main/java/edu/tum/ase/project/DeliveryServiceApplication.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = {BoxRepository.class})
+@EnableEurekaClient
 public class DeliveryServiceApplication implements CommandLineRunner {
 
     @Autowired
--- deliveryService/src/main/java/edu/tum/ase/project/repository/BoxRepository.java
+++ deliveryService/src/main/java/edu/tum/ase/project/repository/BoxRepository.java
@@ -6,7 +6,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import java.util.Collection;
 import java.util.Optional;
 
 public interface BoxRepository extends MongoRepository<Box, ObjectId> {
