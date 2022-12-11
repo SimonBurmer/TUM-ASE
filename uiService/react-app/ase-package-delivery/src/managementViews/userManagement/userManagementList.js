a6bf61c15b27e8f9a9897329e624b0f49ea9f0bd
Felix Friedrich <ge42fih@mytum.de>
2022-12-11 16:37:33 +0100

    Basic Setup of User, Box and Delivery Views

--- uiService/react-app/ase-package-delivery/src/managementViews/userManagement/userManagementList.js
+++ uiService/react-app/ase-package-delivery/src/managementViews/userManagement/userManagementList.js
@@ -4,29 +4,28 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import EditIcon from '@mui/icons-material/Edit';
 import PersonIcon from '@mui/icons-material/Person';
-import {Divider} from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 function generate() {
     return users.map((value) => (
             <ListItem
                 secondaryAction={
+                <List>
                     <IconButton edge="end" aria-label="edit">
                         <EditIcon/>
-                        <Typography variant="h6" component="div">
-                            EDIT
-                        </Typography>
                     </IconButton>
+                    <IconButton sx={{ml:2}} edge="end" aria-label="delete">
+                        <DeleteIcon/>
+                    </IconButton>
+                </List>
                 }
             >
                 <ListItemAvatar>
@@ -36,13 +35,13 @@ function generate() {
                 </ListItemAvatar>
                 <ListItemText
                     primary= {value}
-                    secondary={'Secondary text'}
+                    secondary={'Email als UUID'}
                 />
             </ListItem>
         )
     );
 }
-
+//TODO Liste and Objekten an users übergeben und generate und IntercativeList entsprechend anpassen
 let users= ["Peter", "Simon", "Simon", "Leonard", "Felix"]
 
 const Demo = styled('div')(({ theme }) => ({
@@ -52,12 +51,11 @@ const Demo = styled('div')(({ theme }) => ({
 export default function InteractiveList() {
     const [dense, setDense] = React.useState(false);
 
-
     return (
         <Box sx={{ flexGrow: 1, maxWidth: 1500 }}>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-                    <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                    <Typography sx={{ mt: 4, mb: 2 , ml:3}} variant="h6" component="div">
                         Users
                     </Typography>
                     <Demo>
--- uiService/react-app/ase-package-delivery/src/managementViews/userManagement/userManagementOverview.js
+++ uiService/react-app/ase-package-delivery/src/managementViews/userManagement/userManagementOverview.js
@@ -1,11 +1,22 @@
-import React from "react";
-import {List} from "@mui/material";
+import React, {useState} from "react";
 import InteractiveList from "./userManagementList";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import NewUserFormDialog from "./newUserFormDialog";
+
+
+
+
 
 export default function UserManagementOverview () {
+
     return (
         <div>
+            <Box>
+                <NewUserFormDialog ></NewUserFormDialog>
+            </Box>
             <InteractiveList></InteractiveList>
+
         </div>
     )
 }
