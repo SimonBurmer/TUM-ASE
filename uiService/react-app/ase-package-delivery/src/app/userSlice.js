e44dfa5192bd38a10e1b14d969753f5d75d542bc
Felix Friedrich <ge42fih@mytum.de>
2022-12-16 11:15:13 +0100

    Frontend requests publicKey

--- uiService/react-app/ase-package-delivery/src/app/userSlice.js
+++ uiService/react-app/ase-package-delivery/src/app/userSlice.js
@@ -1,4 +1,6 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+import {fetchQuoteAsync} from "../views/quote/quoteSlice";
 
 //TODO den default auf null setzten und bei login auf die rolle des angemeldeten users setzten
 const initialState = {
@@ -17,8 +19,45 @@ export const userSlice = createSlice({
             state.apiToken = "";
             state.userRole = "";
         },
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(postUserAsync().fulfilled, (state, action) => {
+                //hier im store speichern
+            });
     },
 })
+
+export const postUserAsync = createAsyncThunk(
+    'user/postUser',
+    async (payload) => {
+        const api = axios.create({baseURL: 'http://localhost:10789'}) //TODO move to specific file for constants
+        const publicKey = api.get('/auth/pkey')
+        let encryptedPassword =
+        await publicKey.then((key) => {
+            let rsaKey = Jose.Utils.importRsaPublicKey({"e": parseInt(key.e), "n": key.n}, "RSA-OAEP");
+            console.log("Key built");
+            return rsaKey;
+        })
+            .then(async (rsaKey) => {
+
+                let cryptographer = await new Jose.WebCryptographer();
+                let encrypter = await new Jose.JoseJWE.Encrypter(cryptographer, rsaKey);
+
+                let password = encrypter.encrypt(payload.password)
+                // TODO: Replace encryptAct with a command to encrypt a password written in JSON (i.e., {" password": password}) pw = await encryptAct
+
+                await console.log(`encrypted pw: ${password}`);
+                return password
+
+            });
+        const response = await api.post('/auth', {email: payload.email, password_enc: encryptedPassword})
+        console.log("Login successful" + response.data)
+        return response.data.content
+
+    }
+);
 
 export default userSlice.reducer
 export const selectUserRole = (state) => state.role.userRole;
