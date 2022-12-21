f7a51509bfb5ff5382bc112bad92009e88431ac2
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2022-12-21 15:57:17 +0100

    Clean up dependency management and fix broken tests

--- authService/src/main/java/edu/tum/ase/authService/AuthServiceApplication.java
+++ authService/src/main/java/edu/tum/ase/authService/AuthServiceApplication.java
@@ -1,19 +1,25 @@
 package edu.tum.ase.authService;
 
 import com.mongodb.client.MongoClient;
-import edu.tum.ase.backendCommon.roles.UserRole;
 import edu.tum.ase.authService.model.AseUser;
 import edu.tum.ase.authService.repository.UserRepository;
+import edu.tum.ase.backendCommon.roles.UserRole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Arrays;
 
 @SpringBootApplication(scanBasePackages = {"edu.tum.ase.backendCommon", "edu.tum.ase.authService"})
 public class AuthServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     MongoClient mongoClient;
@@ -35,16 +41,17 @@ public class AuthServiceApplication implements CommandLineRunner {
     public void run(String... args) {
         log.info("MongoClient = " + mongoClient.getClusterDescription());
 
-        // Only create dummy users when there is no other user in DB
-        if (userRepository.findAll().size() == 0) {
-            AseUser bob = new AseUser("bob@priv.de", bCryptPasswordEncoder.encode("password"), UserRole.CUSTOMER);
-            AseUser alice = new AseUser("alice@apd.de", bCryptPasswordEncoder.encode("test1234"), UserRole.DELIVERER);
-            AseUser kate = new AseUser("kate@apd.de", bCryptPasswordEncoder.encode("xyz"), UserRole.DISPATCHER);
+        if (Arrays.stream(environment.getActiveProfiles()).noneMatch(p -> p.equals("test"))) {
+            // Only create dummy users when there is no other user in DB
+            if (userRepository.findAll().size() == 0) {
+                AseUser bob = new AseUser("bob@priv.de", bCryptPasswordEncoder.encode("password"), UserRole.CUSTOMER);
+                AseUser alice = new AseUser("alice@apd.de", bCryptPasswordEncoder.encode("test1234"), UserRole.DELIVERER);
+                AseUser kate = new AseUser("kate@apd.de", bCryptPasswordEncoder.encode("xyz"), UserRole.DISPATCHER);
 
-            userRepository.save(bob);
-            userRepository.save(alice);
-            userRepository.save(kate);
+                userRepository.save(bob);
+                userRepository.save(alice);
+                userRepository.save(kate);
+            }
         }
     }
-
 }
---
+++ deliveryService/src/test/java/edu/tum/ase/deliveryService/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tum.ase.deliveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
