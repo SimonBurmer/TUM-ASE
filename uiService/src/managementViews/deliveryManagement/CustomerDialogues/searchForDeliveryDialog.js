850457f8db1943800862893ac7e192cd489689d9
Felix Friedrich <ge42fih@mytum.de>
2023-01-29 15:30:44 +0100

    Completion of deliveryManagementListCustomer with current and past deliveries and search function

---
+++ uiService/src/managementViews/deliveryManagement/CustomerDialogues/searchForDeliveryDialog.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import {useState} from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import {useSelector} from "react-redux";
+import {selectDeliveries} from "../../../app/deliverySlice";
+
+export default function SearchForDeliveryDialog() {
+    const [open, setOpen] = React.useState(false);
+    const deliveries = useSelector(selectDeliveries)
+    const [searchedForDelivery, setSearchedForDelivery] = useState("")
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                Track a delivery
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Track your delivery</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To get the current Status of your delivery please enter the Delivery ID.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Delivery-ID"
+                        type="deliveryID"
+                        fullWidth
+                        variant="standard"
+                        onChange={(e) => {
+                            setSearchedForDelivery(e.target.value)
+                        }}
+                    />
+                    <DialogContentText>
+                        The status of your delivery is:
+                        {deliveries.filter(function (delivery) {
+                            return delivery.id === searchedForDelivery;
+                        }).map(function (delivery) {
+                                return delivery.status;
+                            }
+                        )}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
--- uiService/src/managementViews/deliveryManagement/deliveryManagementListCustomer.js
+++ uiService/src/managementViews/deliveryManagement/deliveryManagementListCustomer.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import {styled} from '@mui/material/styles';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -12,6 +13,8 @@ import {useSelector} from "react-redux";
 import {selectDeliveries} from "../../app/deliverySlice";
 import NewRequestErrorDialog from "../requestErrorDialog";
 import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import Button from "@mui/material/Button";
+import SearchForDeliveryDialog from "./CustomerDialogues/searchForDeliveryDialog";
 
 function generate(deliveries) {
     return deliveries.map((delivery) => (
@@ -26,8 +29,8 @@ function generate(deliveries) {
                 </ListItemAvatar>
                 <Typography component={'div'}>
                     <ListItemText
-                        primary={"ID: " + delivery.id}
-                        secondary={"Address: " + delivery.address}
+                        primary={"Delivery-ID: " + delivery.id}
+                        secondary={"Status: " + delivery.status + " | Your Pickup-Box: " + delivery?.box.address}
                     />
                 </Typography>
             </ListItem>
@@ -42,21 +45,57 @@ const Demo = styled('div')(({theme}) => ({
 
 export default function DeliveryManagementListCustomer() {
     const deliveries = useSelector(selectDeliveries)
+    const [deliveryHeader, setDeliveryHeader] = useState("Current Deliveries")
+    const [deliveredDeliveries, setDeliveredDeliveries] = useState("")
+    const [pickedUpDeliveries, setPickedUpDeliveries] = useState("PICKED_UP")
+    const [orderedDeliveries, setOrderedDeliveries] = useState("ORDERED")
+    const [inTargetBoxDeliveries, setInTargetBoxDeliveries] = useState("IN_TARGET_BOX")
+
+    const deliveriesToDisplay = deliveries.filter(function (delivery) {
+        return delivery.status === pickedUpDeliveries || delivery.status === orderedDeliveries || delivery.status === inTargetBoxDeliveries || delivery.status === deliveredDeliveries;
+    }).map(function (delivery) {
+            return delivery;
+        }
+    )
+
     return (
         <Container>
             <NewRequestErrorDialog/>
             <Grid container spacing={2}>
                 <Grid item xs={10}>
                     <Typography sx={{mt: 3, mb: 2, ml: 3}} variant="h6" component="div">
-                        Deliveries
+                        {deliveryHeader}
                     </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                    <SearchForDeliveryDialog/>
                 </Grid>
             </Grid>
             <Demo>
                 <List dense={false}>
-                    {generate(deliveries)}
+                    {generate(deliveriesToDisplay)}
                 </List>
             </Demo>
+            <Button variant="contained" disabled={deliveryHeader === "Received Deliveries"} onClick={() => {
+                setDeliveryHeader("Received Deliveries")
+                setInTargetBoxDeliveries("")
+                setOrderedDeliveries("")
+                setPickedUpDeliveries("")
+                setDeliveredDeliveries("DELIVERED")
+            }
+            }>
+                Show received deliveries
+            </Button>
+            <Button variant="contained" disabled={deliveryHeader === "Current Deliveries"} onClick={() => {
+                setDeliveryHeader("Current Deliveries")
+                setInTargetBoxDeliveries("IN_TARGET_BOX")
+                setOrderedDeliveries("ORDERED")
+                setPickedUpDeliveries("PICKED_UP")
+                setDeliveredDeliveries("")
+            }
+            }>
+                Show current deliveries
+            </Button>
         </Container>
     );
 }
