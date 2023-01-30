63c73d2b9121cf2692e2000197d7e3b2d78479c3
Leonard Husmann <leonard.husmann@tum.de>
2023-01-30 15:39:35 +0100

    Added Dockerfile for email service.

--- emailService/src/main/java/edu/tum/ase/emailservice/service/SendEMailService.java
+++ emailService/src/main/java/edu/tum/ase/emailservice/service/SendEMailService.java
@@ -7,7 +7,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class SendEMailService {
