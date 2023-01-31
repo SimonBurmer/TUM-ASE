52747529cd8217efe047a406f263d637ba9aacb9
Leonard Husmann <leonard.husmann@tum.de>
2023-01-31 17:06:22 +0100

    using legacy login for now.

--- uiService/src/app/currUserSlice.js
+++ uiService/src/app/currUserSlice.js
@@ -68,18 +68,19 @@ export const authUserAsync = createAsyncThunk(
                     console.log("Key built");
                     return rsaKey;
                 })
-                    .then(async (rsaKey) => {
-                        let cryptographer = await new Jose.WebCryptographer();
-                        let encrypter = await new Jose.JoseJWE.Encrypter(cryptographer, rsaKey);
-
-                        console.log(`Start pw encryption`);
-                        let password = await encrypter.encrypt(payload.password)
-
-                        await console.log(`encrypted pw: ${password}`);
-                        return password
-
-                    });
-            await api.post('/auth', {email: payload.email, password_enc: encryptedPassword});
+            //     .then(async (rsaKey) => {
+            //         let cryptographer = await new Jose.WebCryptographer();
+            //         let encrypter = await new Jose.JoseJWE.Encrypter(cryptographer, rsaKey);
+            //
+            //         console.log(`Start pw encryption`);
+            //         let password = await encrypter.encrypt(payload.password)
+            //
+            //         await console.log(`encrypted pw: ${password}`);
+            //         return password
+            //
+            //     });
+            const loginInfo = payload.email + ":" + payload.password
+            await api.post('/auth/legacy', {}, {headers: {"Authorization": "Basic " + btoa(loginInfo)}});
             const userInfo = await api.get('user/current')
             return userInfo.data
         } catch (err) {
